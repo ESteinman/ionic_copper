4e8911fc106ea861cd145f672cf19cd7d7881d22
Erik <erik.steinman@hotmail.com>
2018-08-09 13:01:38 +0200

    add providers Person and Cooper, made spec-files and started testing

---
+++ src/providers/cooper/cooper.spec.ts
@@ -0,0 +1,14 @@
+import { CooperProvider, CooperProvider } from './cooper';
+
+describe("CooperProvider", , () => {
+    let CooperProvider: CooperProvider;
+
+    beforeEach(() => {
+        cooperProvider = new CooperProvider();
+    });
+
+    it('should create the cooper provider', () => {
+        expect(cooperProvider).toBeTruthy();
+        expect(cooperProvider)
+    })
+})
