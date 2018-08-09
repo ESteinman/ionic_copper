47bde44d4bd8411c81b91012ab6062c4b1d664de
Erik <erik.steinman@hotmail.com>
2018-08-09 16:55:30 +0200

    add home.specs and add tests, view works

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -1,9 +1,10 @@
 import { HomePage } from "./home";
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, async, inject } from "@angular/core/testing";
 import { IonicModule, Platform, NavController } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock } from "ionic-mocks";
+import { PersonProvider } from '../../providers/person/person'
 
 describe("HomePage", () => {
   let homepage;
@@ -18,7 +19,8 @@ describe("HomePage", () => {
         { provide: Platform, useFactory: () => PlatformMock.instance() },
         { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
         { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
-        { provide: NavController, useFactory: () => NavControllerMock.instance() }
+        { provide: NavController, useFactory: () => NavControllerMock.instance() },
+        { PersonProvider }
       ]
     }).compileComponents();
   }));
@@ -54,5 +56,18 @@ describe("HomePage", () => {
       expect(homepage.calculate).toBeTruthy();
   });
 
-  it('calculate function ')
+  it('calculate function should call person provider doAssessment function', inject(
+    [PersonProvider],
+    person => {
+      homepage.user = { age: 25, gender: "female", distance: 2500 };
+      spyOn(person, "doAssessment").and.returnValue("Above average");
+
+      homepage.calculate();
+
+      expect(person.doAssessment).toHaveBeenCalled();
+      expect(person.doAssessment).toHaveBeenCalledWith(2500);
+      expect(person.age).toEqual(25);
+      expect(person.gender).toEqual("female");
+    }
+  ));
 });
