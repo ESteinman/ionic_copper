4d1787dce2942c86dee4824b90ff7b246e25b714
Erik <erik.steinman@hotmail.com>
2018-08-10 13:44:47 +0200

    getLabels and getCount-functions defined, ionViewDidLoad formula add .labels

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -14,7 +14,7 @@ export class ResultsPage {
   radarChartType: string = 'radar';
 
   view: string = 'data';
-  
+
   constructor(
     private performanceData: PerformanceDataProvider,
     public navCtrl: NavController, 
@@ -24,7 +24,35 @@ export class ResultsPage {
   ionViewDidLoad() {
     this.performanceData
       .getResults()
-      .subscribe(data => (this.results = data.entries));
+      .subscribe(data => {this.results = data.entries;
+      this.labels = this.getLabels(this.results);
+
+      this.labels.forEach(label => {
+        this.data.push(this.getCount(this.results, label));
+      })
+    });
+  }
+
+  getLabels(collection: any) {
+    let uniqueLabels = [];
+
+    collection.forEach(entry => {
+      if (entry.data.message && uniqueLabels.indexOf(entry.data.message) === -1) {
+        uniqueLabels.push(entry.data.message);
+      }
+    })
+
+    return uniqueLabels;
+  }
+
+  getCount(collection: any, value:any) {
+    let count = 0;
+
+    collection.forEach(entry => {
+      count += entry.data.message == value ? 1 : 0;
+    })
+
+    return count;
   }
 
 }
