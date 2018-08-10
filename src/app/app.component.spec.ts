f08dedb143820b25b7b790f1e24747ab51c2fff3
Erik <erik.steinman@hotmail.com>
2018-08-10 11:21:39 +0200

    add injects to app.module.ts, add init-function to app.component.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,25 @@
 import { MyApp } from './app.component'
-import { TestBed } from '@angular/core/testing'
+import { TestBed, async, inject } from '@angular/core/testing'
 import { IonicModule, Platform } from 'ionic-angular';
 import { PlatformMock, StatusBarMock, SplashScreenMock } from 'ionic-mocks'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Http, BaseRequestOptions, RequestMethod } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { Angular2TokenService } from 'angular2-token';
 
 describe ('AppComponent', () => {
     let fixture, component;
 
+    let signInData = {
+        email: 'test@test.com',
+        password: 'password',
+        userType: String
+    };
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [
-                MyApp
-            ],
+            declarations: [MyApp],
             imports: [
                 IonicModule.forRoot(MyApp)
             ],
